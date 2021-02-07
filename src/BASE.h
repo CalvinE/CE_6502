01402bde26245061e77eb91b4b8e2905eda01817
Calvin Echols <calvin.echols@gmail.com>
2021-02-06 20:22:15 -0500

    minor changes

---
+++ src/BASE.h
@@ -0,0 +1,6 @@
+#ifndef _CE_6502_H_
+#define _CE_6502_H_
+    #define CE_6502_VERSION_MAJOR 0
+    #define CE_6502_VERSION_MINOR 1
+    #define CE_6502_VERSION_PATCH 0
+#endif
--- src/CE_6502.cpp
+++ src/CE_6502.cpp
@@ -1,3 +1,4 @@
+#include "BASE.h"
 #include "CE_6502.h"
 #include <iostream>
 #include <string>
